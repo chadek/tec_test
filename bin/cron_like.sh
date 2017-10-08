572930b104101e85899292bc0561053de84a73cd
FABABY <meyer.fabrice@gmx.fr>
2017-10-08 10:00:21 +0200

    Fixing bug with directories when running cron task (bad folder init)

--- bin/cron_like.sh
+++ bin/cron_like.sh
@@ -4,12 +4,12 @@ readonly SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 while true;do
     date
-    ./bin/cron_task.sh ogv  480p  >> "${SCRIPT_PATH}/../cron_log/cron_task_ogv_480p.log"   2>&1 &
-    ./bin/cron_task.sh ogv  720p  >> "${SCRIPT_PATH}/../cron_log/cron_task_ogv_720p.log"    2>&1 &
-    ./bin/cron_task.sh ogv 1080p  >> "${SCRIPT_PATH}/../cron_log/cron_task_ogv_1080p.log"   2>&1 &
-    ./bin/cron_task.sh mp4  480p  >> "${SCRIPT_PATH}/../cron_log/cron_task_mp4_480p.log"    2>&1 &
-    ./bin/cron_task.sh mp4  720p  >> "${SCRIPT_PATH}/../cron_log/cron_task_mp4_720p.log"    2>&1 &
-    ./bin/cron_task.sh mp4 1080p  >> "${SCRIPT_PATH}/../cron_log/cron_task_mp4_1080p.log"   2>&1 
+    ./bin/cron_task.sh ogv  480p  >> ${SCRIPT_PATH}/../cron_log/cron_task_ogv_480p.log   2>&1 &
+    ./bin/cron_task.sh ogv  720p  >> ${SCRIPT_PATH}/../cron_log/cron_task_ogv_720p.log    2>&1 &
+    ./bin/cron_task.sh ogv 1080p  >> ${SCRIPT_PATH}/../cron_log/cron_task_ogv_1080p.log   2>&1 &
+    ./bin/cron_task.sh mp4  480p  >> ${SCRIPT_PATH}/../cron_log/cron_task_mp4_480p.log    2>&1 &
+    ./bin/cron_task.sh mp4  720p  >> ${SCRIPT_PATH}/../cron_log/cron_task_mp4_720p.log    2>&1 &
+    ./bin/cron_task.sh mp4 1080p  >> ${SCRIPT_PATH}/../cron_log/cron_task_mp4_1080p.log   2>&1 
 
     sleep 300
 done
